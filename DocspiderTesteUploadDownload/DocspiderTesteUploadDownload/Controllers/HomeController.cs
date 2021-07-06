41ca4b4807c058f1045b5d649121e633b66a4fd3
sylvio labriola <sylvioez@hotmail.com>
2021-07-05 22:56:20 -0300

    Criação do projeto.

---
+++ DocspiderTesteUploadDownload/DocspiderTesteUploadDownload/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using DocspiderTesteUploadDownload.Models;
+using DocspiderTesteUploadDownload.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DocspiderTesteUploadDownload.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<Upload> upload = UploadService.GetUpload();
+
+        public ActionResult Index()
+        {
+            return View(upload);
+        }
+
+        public ActionResult Incluir()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ DocspiderTesteUploadDownload/DocspiderTesteUploadDownload/Models/UploadArquivo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace DocspiderTesteUploadDownload.Models{
+    public class Upload{
+        public int Id_Upload { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public string Arquivo { get; set; }
+        public string Nome_Do_Arquivo { get; set; }
+        public DateTime DataArquivo { get; set; }
+    }
+}
