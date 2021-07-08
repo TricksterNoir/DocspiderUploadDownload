9f622616cea5c9ecd2a7425e1683b80f641d1d35
sylvio labriola <sylvioez@hotmail.com>
2021-07-07 21:22:09 -0300

    Alteração Projeto

---
+++ DocspiderTesteUploadDownload/Presentation.DocspiderTesteUploadDownload/Models/MUpload.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Presentation.DocspiderTesteUploadDownload.Models
+{
+    public class MUpload
+    {
+        public int Id_Upload { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public byte Arquivo { get; set; }
+        public string Nome_Do_Arquivo { get; set; }
+        public DateTime DataCriacao { get; set; }
+    }
+}
